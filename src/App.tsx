194d9663563c45ce407192166519bcfa1539f2ea
Surjith <mahindhiran005@gmail.com>
2025-04-04 22:46:14 +0530

    detail component completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
 import List from "./components/list/List";
 import Detail from "./components/detail/Detail";
 import Chat from "./components/chat/Chat";
+import { useEffect, useState } from "react";
 
 function App() {
+  //   const [online, setOnline] = useState(navigator.onLine);
+  //   console.log(navigator);
+
+  // useEffect(() => {
+  //     const updateStatus = () => {
+  //       if (!navigator.onLine || document.hidden) {
+  //         setOnline(false);
+  //         console.log("Offline or hidden");
+  //       } else {
+  //         setOnline(true);
+  //         console.log("Online and visible");
+  //       }
+  //     };
+
+  //     window.addEventListener("online", updateStatus);
+  //     window.addEventListener("offline", updateStatus);
+  //     document.addEventListener("visibilitychange", updateStatus);
+
+  //     updateStatus();
+  //     const handleUnload = () => {
+  //       alert("hello"); // Notify backend before closing
+  //     };
+
+  //     window.addEventListener("beforeunload", handleUnload);
+  //     return () => {
+  //       window.removeEventListener("online", updateStatus);
+  //       window.removeEventListener("offline", updateStatus);
+  //       document.removeEventListener("visibilitychange", updateStatus);
+  //     };
+  //   }, []);
+
   return (
     <div className="flex p-3 gap-5 h-[90vh] w-[90vw] bg-[rgb(17,25,40,0.75)] rounded-lg *:border-r-gray-600 *:border-r *:p-2">
       <List />
--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -1,7 +1,46 @@
 import { JSX } from "react";
+import { BiSolidUserCircle } from "react-icons/bi";
+import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleUp } from "react-icons/fa6";
 
 const Detail = (): JSX.Element => {
-  return <div className="w-1/5 border-none">Detail</div>;
+  return (
+    <div className="w-1/5 border-none space-y-10">
+      <div className="flex flex-col items-center gap-2">
+        <BiSolidUserCircle size={90} />
+        <span className="text-xl font-medium">Ashok kumar</span>
+        <span className="tracking-wide text-sm text-gray-400 font-medium">
+          Grateful for sunrise and sunset🌄
+        </span>
+      </div>
+      <div className="*:flex *:justify-between *:items-center flex flex-col gap-5">
+        <div>
+          <span>Chat settings</span>
+          <FaAngleUp size={20} className="bg-gray-600 rounded-full p-1.5" />
+        </div>
+        <div>
+          <span>Privacy & help</span>
+          <FaAngleUp size={20} className="bg-gray-600 rounded-full p-1.5" />
+        </div>
+        <div>
+          <span>Shared Photos</span>{" "}
+          <FaAngleUp size={20} className="bg-gray-600 rounded-full p-1.5" />
+        </div>
+        <div>
+          <span>Shared files</span>{" "}
+          <FaAngleUp size={20} className="bg-gray-600 rounded-full p-1.5" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <button className="bg-[rgba(255,0,0,0.40)] p-2 rounded-lg ">
+          Block User
+        </button>
+        <button className="bg-[rgba(0,32,255,0.45)] p-2 rounded-lg">
+          Log out
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
